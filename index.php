fb43b8cc7a866e7cf4b84acff98dfcb8a257862d
Julia Kulla-Mader <64906+juliakm@users.noreply.github.com>
2025-11-20 17:29:38 -0500

    Refactor index.php to output HTML structure and set appropriate headers

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+declare(strict_types=1);
 
-echo "Hello World!";
+header('Content-Type: text/html; charset=UTF-8');
+header('X-UA-Compatible: ie=edge');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World - PHP Sample</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
