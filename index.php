88bf32bb44b0d8863fb4dd1b398a31acf3667f75
Julia Kulla-Mader <64906+juliakm@users.noreply.github.com>
2025-12-02 15:00:34 -0500

    Update README.md and refactor index.php to simplify output format

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 declare(strict_types=1);
 
-header('Content-Type: text/html; charset=UTF-8');
-header('X-UA-Compatible: ie=edge');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World - PHP Sample</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-</body>
-</html>
+header('Content-Type: text/plain; charset=UTF-8');
+header('X-Content-Type-Options: nosniff');
+header('X-Frame-Options: DENY');
+
+echo "Hello World!";
